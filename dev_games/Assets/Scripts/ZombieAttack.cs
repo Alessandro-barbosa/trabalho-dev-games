229bb3e39891a706ce78831d608d42fcc2cf416b
Bernardo Venturotti Braun Rauta Ramos <40549558+bernardovento@users.noreply.github.com>
2024-04-25 11:52:06 -0300

    IA dos Zumbis

---
+++ dev_games/Assets/Scripts/ZombieAttack.cs
@@ -0,0 +1,45 @@
+using Pathfinding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAttack : MonoBehaviour
+{
+    private GameObject target;
+    private Animator zombieAnim;
+    private AIPath AIpath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zombieAnim = GetComponent<Animator>();
+        AIpath = GetComponent<AIPath>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zombieAnim.SetBool("IsMoving", AIpath.canMove);
+
+        if(target != null)
+        {
+            if(Vector3.Distance(target.transform.position, transform.position) > 1.6)
+            {
+                AIpath.canMove = true;
+                target = null;  
+            }
+            Debug.Log(Vector3.Distance(target.transform.position, transform.position));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            target = collision.gameObject;
+            AIpath.canMove = false;
+            Debug.Log(Vector3.Distance(collision.gameObject.transform.position, transform.position));
+            zombieAnim.SetTrigger("RightAttackTrigger");
+        }
+    }
+}
